619eaa53e15806946efda2a448792ed9c2b8600a
Mon <boonnakchayanin@gmail.com>
2024-03-12 20:44:31 +0700

    test1

--- Notifications.js
+++ Notifications.js
@@ -1,76 +1,76 @@
-// import * as Notifications from "expo-notifications";
-// import React, { useEffect } from "react";
-// import { db } from "./firebase/firebase";
+// // import * as Notifications from "expo-notifications";
+// // import React, { useEffect } from "react";
+// // import { db } from "./firebase/firebase";
 
-// export async function schedulePushNotification() {
-//   await Notifications.scheduleNotificationAsync({
-//     content: {
-//       title: "You've got mail! 📬",
-//       body: "Here is the notification body",
-//       data: { data: "goes here" },
-//     },
-//     trigger: { seconds: 2 },
-//   });
-// }
-// async function registerForPushNotificationsAsync() {
-//   let token;
-//   const { status: existingStatus } = await Notifications.getPermissionsAsync();
-//   let finalStatus = existingStatus;
-//   if (existingStatus !== "granted") {
-//     const { status } = await Notifications.requestPermissionsAsync();
-//     finalStatus = status;
-//   }
-//   if (finalStatus !== "granted") {
-//     alert("Failed to get push token for push notification!");
-//     return;
-//   }
-//   token = (await Notifications.getExpoPushTokenAsync(db)).data;
-//   token = (
-//     await Notifications.getExpoPushTokenAsync({
-//       apiKey: "AIzaSyBxLPRq44gKYyKuyiQreFN1NR0kJTGlC1Q",
-//       authDomain: "tbdfridge.firebaseapp.com",
-//       projectId: "tbdfridge",
-//       storageBucket: "tbdfridge.appspot.com",
-//       messagingSenderId: "805582660558",
-//       appId: "1:805582660558:web:bd353524e9776cfaebc19f",
-//     })
-//   ).data;
-//   console.log(token);
-//   // ที่นี่คุณสามารถส่ง token ไปยังเซิร์ฟเวอร์ของคุณหากจำเป็น
-//   return token;
-// }
+// // export async function schedulePushNotification() {
+// //   await Notifications.scheduleNotificationAsync({
+// //     content: {
+// //       title: "You've got mail! 📬",
+// //       body: "Here is the notification body",
+// //       data: { data: "goes here" },
+// //     },
+// //     trigger: { seconds: 2 },
+// //   });
+// // }
+// // async function registerForPushNotificationsAsync() {
+// //   let token;
+// //   const { status: existingStatus } = await Notifications.getPermissionsAsync();
+// //   let finalStatus = existingStatus;
+// //   if (existingStatus !== "granted") {
+// //     const { status } = await Notifications.requestPermissionsAsync();
+// //     finalStatus = status;
+// //   }
+// //   if (finalStatus !== "granted") {
+// //     alert("Failed to get push token for push notification!");
+// //     return;
+// //   }
+// //   token = (await Notifications.getExpoPushTokenAsync(db)).data;
+// //   token = (
+// //     await Notifications.getExpoPushTokenAsync({
+// //       apiKey: "AIzaSyBxLPRq44gKYyKuyiQreFN1NR0kJTGlC1Q",
+// //       authDomain: "tbdfridge.firebaseapp.com",
+// //       projectId: "tbdfridge",
+// //       storageBucket: "tbdfridge.appspot.com",
+// //       messagingSenderId: "805582660558",
+// //       appId: "1:805582660558:web:bd353524e9776cfaebc19f",
+// //     })
+// //   ).data;
+// //   console.log(token);
+// //   // ที่นี่คุณสามารถส่ง token ไปยังเซิร์ฟเวอร์ของคุณหากจำเป็น
+// //   return token;
+// // }
 
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: false,
-//     shouldSetBadge: false,
-//   }),
-// });
+// // Notifications.setNotificationHandler({
+// //   handleNotification: async () => ({
+// //     shouldShowAlert: true,
+// //     shouldPlaySound: false,
+// //     shouldSetBadge: false,
+// //   }),
+// // });
 
-// export function useNotification() {
-//   useEffect(() => {
-//     registerForPushNotificationsAsync();
-//     // ฟังก์ชันที่จะถูกเรียกเมื่อแอปพลิเคชันอยู่ใน foreground
-//     const foregroundSubscription =
-//       Notifications.addNotificationReceivedListener((notification) => {
-//         console.log(notification);
-//       });
+// // export function useNotification() {
+// //   useEffect(() => {
+// //     registerForPushNotificationsAsync();
+// //     // ฟังก์ชันที่จะถูกเรียกเมื่อแอปพลิเคชันอยู่ใน foreground
+// //     const foregroundSubscription =
+// //       Notifications.addNotificationReceivedListener((notification) => {
+// //         console.log(notification);
+// //       });
 
-//     // ฟังก์ชันที่จะถูกเรียกเมื่อผู้ใช้คลิกการแจ้งเตือน
-//     const responseSubscription =
-//       Notifications.addNotificationResponseReceivedListener((response) => {
-//         console.log(response);
-//       });
+// //     // ฟังก์ชันที่จะถูกเรียกเมื่อผู้ใช้คลิกการแจ้งเตือน
+// //     const responseSubscription =
+// //       Notifications.addNotificationResponseReceivedListener((response) => {
+// //         console.log(response);
+// //       });
 
-//     return () => {
-//       Notifications.removeNotificationSubscription(foregroundSubscription);
-//       Notifications.removeNotificationSubscription(responseSubscription);
-//     };
-//   }, []);
-// }
+// //     return () => {
+// //       Notifications.removeNotificationSubscription(foregroundSubscription);
+// //       Notifications.removeNotificationSubscription(responseSubscription);
+// //     };
+// //   }, []);
+// // }
 
-// // จำเป็นต้องแทรกโค้ดสำหรับ registerForPushNotificationsAsync ที่นี่
+// // // จำเป็นต้องแทรกโค้ดสำหรับ registerForPushNotificationsAsync ที่นี่
 import * as Notifications from "expo-notifications";
 import { useEffect, useRef, useState } from "react";
 import { Platform } from "react-native";
